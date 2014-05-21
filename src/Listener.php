7ba72fa3839b7e13878d61e98004beb43a35b3f1
Ross Riley <riley.ross@gmail.com>
2014-05-21 12:30:33 +0100

    initial project commit

---
+++ src/Listener.php
@@ -0,0 +1,60 @@
+<?php
+namespace Doctrine\Uploader;
+
+use Sirius\Upload\Handler;
+
+
+class Listener {
+
+    public $fs;
+    public $targets;
+
+    /**
+     * undocumented function
+     *
+     * @param Filesystem
+     * @param $targets - an array keyed on Entity name, eg: My\Entity=>[field1,field2]
+     **/
+
+    public function __construct(Filesystem $fs, $targets = []) {
+        $this->fs = $fs;
+        $this->targets = $targets;
+    }
+
+    public function preUpdate($args) {
+        $entity = $args->getObject();
+        $entityManager = $args->getObjectManager();
+        
+        if(array_key_exists(get_class($entity), $this->targets)) {
+            $fields = $this->targets[get_class($entity)];
+            foreach($fields as $field) {
+                $this->upload($entity, $field);
+            }
+        }
+
+        if ($entity instanceof Entity\Dealer && is_array($entity->logo)) {
+            $entity->logo = $this->upload($entity, "logo");
+        }
+        
+        if ($entity instanceof Entity\Dealer && is_array($entity->image)) {
+            $entity->image = $this->upload($entity, "image");
+        }
+
+
+
+    }
+
+    public function upload($entity, $field) {
+        $raw = $entity->$field;
+        $this->fs->setOverwrite(false);
+        $this->fs->setPrefix($entity->account."/");
+        $result = $this->fs->process($raw);
+        if($result->isValid()) {
+            return $result->name;
+        }
+        $result->clear();
+        return false;
+    }
+
+
+}
